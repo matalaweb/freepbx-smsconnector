c3bfb8586945bab3b3b17757ed2ce33fac45ad6a
Matthew Weber <matt@temprapp.com>
2024-09-05 11:24:41 -0500

    Add inbound handling.

--- providers/provider-Brck.php
+++ providers/provider-Brck.php
@@ -77,6 +77,67 @@ class Brck extends providerBase
 
     public function callPublic($connector)
     {
+        $config = $this->getConfig($this->nameRaw);
+
+        if ($_SERVER['REQUEST_METHOD'] !== "POST") {
+            return 405;
+        }
+
+        $postdata = file_get_contents("php://input");
+        $sms      = json_decode($postdata)[0];
+
+        freepbx_log(FPBX_LOG_INFO, sprintf(_('Webhook (%s) in: %s'), $this->nameRaw, print_r($postdata, true)));
+
+        if (empty($sms)) {
+            return 403;
+        }
+
+        if (isset($sms)) {
+            if (isset($sms->type) && ($sms->type == 'message-received')) {
+
+                $from = ltrim($sms->message->from, '+'); // strip + if exists
+                $to   = ltrim($sms->to, '+'); // The sms->to will always just be one number, but if we wanted to support group messaging we'd need to look at sms->message->to which holds an array of all numbers in the convo
+                $text = $sms->message->text;
+                $emid = $sms->message->id;
+
+                try {
+                    $msgid = $connector->getMessage($to, $from, '', $text, null, null, $emid);
+                } catch (\Exception $e) {
+                    throw new \Exception(sprintf(_('Unable to get message: %s'), $e->getMessage()));
+                }
+
+                if (isset($sms->message->media[0])) {
+                    // Create authentication header/context required to grab media from Bandwidth
+                    $context = stream_context_create([
+                        "http" => [
+                            "header" => "Authorization: Basic " . base64_encode($config['api_token'] . ":" . $config['api_secret'])
+                        ]
+                    ]);
+
+                    foreach ($sms->message->media as $media) {
+                        $img = file_get_contents($media, false, $context);
+                        $purl = parse_url($media);
+                        $name = $msgid . basename($purl['path']);
+
+                        try {
+                            $connector->addMedia($msgid, $name, $img);
+                        } catch (\Exception $e) {
+                            throw new \Exception(sprintf(_('Unable to store MMS media: %s'), $e->getMessage()));
+                        }
+                    }
+                }
+
+                $connector->emitSmsInboundUserEvt($msgid, $to, $from, '', $text, null, 'Smsconnector', $emid);
+            } else if (isset($sms->type)) {
+                // Likely a callback for delivery status
+                // See https://dev.bandwidth.com/docs/messaging/webhooks/
+                // These can be disabled when setting up the application within Bandwidth
+                // It would be good to support these in the future
+                // We still reply with a 202 to appease Bandwidth
+                freepbx_log(FPBX_LOG_INFO, sprintf(_('Incoming message of unknown type %s. Contents: %s'), $sms->type, print_r($sms, true)));
+            }
+        }
+
         return 202;
     }
 }
